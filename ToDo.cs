b1235268be1e93a5906ecae2e44a66d3c8eb4745
Vasco-PC <kravchuk.vasilii@yandex.ru>
2022-01-04 00:49:17 +0300

    Unity. Tasks
    Added Unity project.
    Added tasks,

---
+++ ToDo.cs
@@ -0,0 +1,8 @@
+Unity Version: 2020.3.25f1
+Link to architecture:
+https://docs.google.com/presentation/d/16oljcyFnAJJktUumtk3kZgDasXJJmIvOz_t0VuU9BQw/edit?usp=sharing
+
+Tasks:
+2.1. Controlled ship
+2.2. Fire with timer
+2.3. Enemy asteroid (single size)
