db7b581b820396165cbecad62583f223206bb763
Vasco-PC <kravchuk.vasilii@yandex.ru>
2022-01-05 01:49:33 +0300

    Tasks
    Separated tasks

--- ToDo.cs
+++ ToDo.cs
@@ -2,7 +2,12 @@ Unity Version: 2020.3.25f1
 Link to architecture:
 https://docs.google.com/presentation/d/16oljcyFnAJJktUumtk3kZgDasXJJmIvOz_t0VuU9BQw/edit?usp=sharing
 
-Tasks:
-2.1. Controlled ship
+================================================================================
+TASKS
+================================================================================
+VascoDaKrava:
 2.2. Fire with timer
+
+Hunter:
+2.1. Controlled ship
 2.3. Enemy asteroid (single size)
