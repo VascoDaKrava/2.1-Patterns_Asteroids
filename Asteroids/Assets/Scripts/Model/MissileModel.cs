bada602d7d9dfb248cc73487108eab0f3cfa73d7
Vasco-PC <kravchuk.vasilii@yandex.ru>
2022-01-12 13:34:38 +0300

    Missile speed
    Missile speed was increased

--- Asteroids/Assets/Scripts/Model/MissileModel.cs
+++ Asteroids/Assets/Scripts/Model/MissileModel.cs
@@ -10,7 +10,7 @@ namespace Asteroids
         #region Properties
 
         public int Damage { get; } = 10;
-        public float Speed { get; } = 10.0f;
+        public float Speed { get; } = 20.0f;
 
         #endregion
 
