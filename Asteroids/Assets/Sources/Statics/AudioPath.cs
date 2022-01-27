cee9c86906b4a0555b63a517e499a0507bc10dad
Vasco-PC <kravchuk.vasilii@yandex.ru>
2022-01-27 13:26:40 +0300

    SoundResources
    AddSoundResources

---
+++ Asteroids/Assets/Sources/Statics/AudioPath.cs
@@ -0,0 +1,32 @@
+using System.IO;
+using UnityEngine;
+
+
+namespace Asteroids
+{
+    public static class AudioPath
+    {
+
+        #region Fields
+
+        public const string MUSIC_MENU = "MenuMusic.mp3";
+
+        public const string MUSIC_GAME = "GameMusic.mp3";
+
+        public const string SFX_BUTTON_ENTER = "ButtonEnter";
+
+        public const string SFX_BUTTON_PRESS = "ButtonPress";
+
+        #endregion
+
+
+        #region Properties
+
+        public static string FOLDER_MUSIC { get; } = Path.Combine(Application.dataPath, "Audio", "Music");
+
+        public static string FOLDER_SFX { get; } = Path.Combine(Application.dataPath, "Audio", "Effects");
+
+        #endregion
+
+    }
+}
