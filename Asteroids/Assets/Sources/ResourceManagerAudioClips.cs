f63acf8b62a98492159ae60333ab555afee8ea2a
Artur <yermak01artur@gmail.com>
2022-01-31 19:02:18 +0200

    Added AudioClips

--- Asteroids/Assets/Sources/ResourceManagerAudioClips.cs
+++ Asteroids/Assets/Sources/ResourceManagerAudioClips.cs
@@ -19,6 +19,20 @@ namespace Asteroids
         
         public AudioClip AudioClipButtonClick { get; private set; }
 
+        public AudioClip AudioClipEnemyExplosion { get; private set; }
+
+        public AudioClip AudioClipHitAsteroid { get; private set; }
+
+        public AudioClip AudioClipHitEnemyShip { get; private set; }
+
+        public AudioClip AudioClipMovingEnemyShip { get; private set; }
+
+        public AudioClip AudioClipMovingPlayerShip { get; private set; }
+
+        public AudioClip AudioClipSpawnEnemy { get; private set; }
+
+        public AudioClip AudioClipStartRocket { get; private set; }
+
         #endregion
 
 
@@ -32,6 +46,15 @@ namespace Asteroids
             AudioClipGame = Resources.Load<AudioClip>(ResourcesPathAudio.MUSIC_GAME);
             AudioClipButtonEnter = Resources.Load<AudioClip>(ResourcesPathAudio.SFX_BUTTON_ENTER);
             AudioClipButtonClick = Resources.Load<AudioClip>(ResourcesPathAudio.SFX_BUTTON_CLICK);
+
+            AudioClipEnemyExplosion = Resources.Load<AudioClip>(ResourcesPathAudio.SFX_ENEMY_EXPLOSION);
+            AudioClipHitAsteroid = Resources.Load<AudioClip>(ResourcesPathAudio.SFX_HIT_ASTEROID);
+            AudioClipHitEnemyShip = Resources.Load<AudioClip>(ResourcesPathAudio.SFX_HIT_ENEMY_SHIP);
+            AudioClipMovingEnemyShip = Resources.Load<AudioClip>(ResourcesPathAudio.SFX_MOVING_ENEMY_SHIP);
+            AudioClipMovingPlayerShip = Resources.Load<AudioClip>(ResourcesPathAudio.SFX_MOVING_PLAYER_SHIP);
+            AudioClipSpawnEnemy = Resources.Load<AudioClip>(ResourcesPathAudio.SFX_SPAWN_ENEMY);
+            AudioClipStartRocket = Resources.Load<AudioClip>(ResourcesPathAudio.SFX_START_ROCKET);
+
         }
 
         #endregion
